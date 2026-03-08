e5011f921ea3471ee5e6cd357a91ad98b47f353f
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-03-08 21:20:14 +0530

    Updated app.js to serve static files and support React Router.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require("cors")
 const { apiLimiter, authLimiter } = require("../src/middleware/rateLimiter.middleware");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
+    origin: true,
+    credentials: true
 }));
+
+// correct public path
+const publicPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicPath));
 
 app.use("/api/auth/login", authLimiter)
 app.use("/api/auth/register", authLimiter)
@@ -31,6 +37,11 @@ app.use("/api/history", historyRouter);
 app.use("/api/movies", movieRouter);
 app.use("/api/users", userRouter);
 app.use("/api/test", testRoutes);
+
+// React router support
+app.use((req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
 
 // app.use(handleError);
 
